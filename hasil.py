30c2a881371e9c43083dc5743b0dd6c640392b62
amndprilll <146147557+amndprilll@users.noreply.github.com>
2023-12-06 19:29:11 +0700

    hasil

---
+++ hasil.py
@@ -0,0 +1,26 @@
+import bangundatar
+
+bangundatar.luasSegitiga(3 , 6)
+bangundatar.luasPersegi(8)
+bangundatar.luasPersegiPanjang(3 , 4)
+bangundatar.luasBelahKetupat(5 , 4)
+bangundatar.luasJajarGenjang(3 , 6)
+
+bangundatar.kelilingSegitiga(15)
+bangundatar.kelilingPersegi(4)
+bangundatar.kelilingPersegiPanjang(3, 4)
+bangundatar.KelilingBelahKetupat(5)
+bangundatar.kelilingJajarGenjang(10, 5)
+
+import operasiMtk
+
+operasiMtk.tambah(4 , 6)
+operasiMtk.kurang(5, 2)
+operasiMtk.bagi(10, 5)
+operasiMtk.kali(2, 6)
+operasiMtk.pangkat(2 , 8)
+operasiMtk.akar(100)
+operasiMtk.log(100)
+operasiMtk.sin(6)
+operasiMtk.cos(6)
+operasiMtk.tan(6)
